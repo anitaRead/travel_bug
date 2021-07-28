bdedc8284e800899f8740b0952e1b98cea080f63
anitaRead <areadman91@gmail.com>
2021-07-28 11:15:42 +0100

    Display red list countries - issue: not dynamic

--- controllers/home.js
+++ controllers/home.js
@@ -1,20 +1,49 @@
 var https = require("https");
+var countryList = require("country-list");
 
 var HomeController = {
     Index: function(req, res) {
         res.render('search/test', { title: 'Test' });
-      },
+    },
+
     List: function(req, res) {
-      var redUrl = "https://www.gov.uk/api/content/guidance/red-amber-and-green-list-rules-for-entering-england#red-list"
-      https.get(redUrl, function(res){
-        console.log(res.statusCode);
-        res.on("data", function(data){
-          var countryData = JSON.parse(data);
-          console.log(countryData.details.body);
+
+      var url = "https://www.gov.uk/api/content/guidance/red-amber-and-green-list-rules-for-entering-england";
+      
+
+      https.get(url, function(response){
+        var result = "";
+        
+        response.on("data", function(data){
+          result += data;
+        })
+
+        response.on("end", function(){
+          var resultJSON = JSON.parse(result);
+          var pageData = resultJSON.details.body;
+
+          pageData = pageData.replace(/(<([^>]+)>)/gi, "");
+
+          var countriesRef = countryList.getNames();
+
+          // var redStartInd = pageData.search("Afghanistan");
+          // var redEndInd = pageData.search("Zimbabwe");
+
+          var redList = pageData.slice(830, 3260);
+          var filteredRedList = [];
+
+          for(var i=0; i<countriesRef.length; i++) {
+            var redCountry = redList.search(countriesRef[i]);
+            if(redCountry > -1) {
+              filteredRedList.push(countriesRef[i]);
+            }
+          }
+          
+          filteredRedList = filteredRedList.sort();
+          
+          res.render('explore/index', { redListData: filteredRedList })
         })
       })
-
-      res.render('explore/index');
       
     }
     
