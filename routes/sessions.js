d0e66bbb31c1f4fb985c6d3ce96fd1e0c5713698
Kelvin Munro <kmunro4@ford.com>
2021-08-03 15:24:16 +0100

    sign in and sign up seem to work, no tests currently work, more work required when merged main into it

--- routes/sessions.js
+++ routes/sessions.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 var HomeController = require('../controllers/home');
 
-// router.get('/signup', HomeController.SignupPage)
-// router.post('/new', HomeController.Create);
+router.get('/signup', HomeController.SignupPage)
+router.post('/new', HomeController.Create);
 router.get('/', HomeController.SigninPage);
 router.post('/new', HomeController.Signin);
 router.post('/end', HomeController.Signout);
